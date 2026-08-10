54317dfdab9b4ddcf7b1ccd85a9ada012892cf86
peterbebawy <115377883+peterbebawy@users.noreply.github.com>
2026-08-10 22:27:37 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyCikAWrMtiLw0WAYCH7I4iG6Wv2tMsEt9w",
   authDomain: "customer-care-841fe.firebaseapp.com",
-  databaseURL: "https://customer-care-841fe-default-rtdb.firebaseio.com",
   projectId: "customer-care-841fe",
   storageBucket: "customer-care-841fe.firebasestorage.app",
   messagingSenderId: "167461661789",
-  appId: "1:167461661789:web:a1141fccdf4650b84eb1b4"
+  appId: "1:167461661789:web:a1141fccdf4650b84eb1b4",
+  measurementId: "G-EQ650CB20M"
 };
-
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+export { db, auth };
